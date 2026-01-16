a605fa7f104b3cf6d7a9493cbebd924c72e28392
mahibpatel2464 <mahibpatel2464@gmail.com>
2026-01-16 22:16:44 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <ProfileCard
+      name="Mahi Patel"
+      age={21}
+      image="https://via.placeholder.com/100"
+    >
+      <p>React Learner 🚀</p>
+    </ProfileCard>
+  );
+}
+
+export default App;
